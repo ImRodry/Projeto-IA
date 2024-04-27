2c44c527b6ad880462d923e41a8e458b7c2b0b1d
Rodry <38259440+ImRodry@users.noreply.github.com>
2024-04-27 13:19:51 +0100

    feat: first pacman game

--- src/algoritmo/GeneticAlgorithm.java
+++ src/algoritmo/GeneticAlgorithm.java
@@ -14,8 +14,8 @@ import utils.Commons;
 
 public class GeneticAlgorithm {
 	private static final BoardTypes BOARD_TYPE = BoardTypes.PACMAN;
-	private static final int POPULATION_SIZE = 100;
-	private static final int NUM_GENERATIONS = 1000;
+	private static final int POPULATION_SIZE = BOARD_TYPE == BoardTypes.BREAKOUT  ? 100  : 10; // The file size for 100 Pacman individuals is too large 
+	private static final int NUM_GENERATIONS = 10;
 	private static final double MUTATION_RATE = 0.01;
 	private static final int TOURNAMENT_SIZE = 10;
 	private static final String FILENAME = BOARD_TYPE == BoardTypes.BREAKOUT ? "breakout.txt" : "pacman.txt";
